0543dd12b2049141c2c3c4b9ceb53d183ffb5d7a
kimJM <kimth5533@naver.com>
2019-11-22 16:22:08 +0900

    DAY33

--- study/DAY26/js/네이버예제.js
+++ study/DAY26/js/네이버예제.js
@@ -98,6 +98,7 @@ $(function(){
         }
     })
 
+
     $('.lf_btn').click(function(){
         var str = $('.num_box1').text()
         var stt = $('.num_box2').text()
@@ -236,9 +237,34 @@ $(function(){
         initBlackMenu();
         setInputCheckbox(selMenu);
     })
-})
-
-
+    //setInterval(함수,시간) : 일정시간마다 함수를 동작시키는 함수 
+    var cnt = 1;
+    setInterval(function(){
+      //  var height = "-18px";
+        ticker('.search_new_box', 500)
+        ticker('.news_cast', 500)
+    },2000);
+//     setInterval(function(){
+//         var height2 = "-20px";
+//         ticker2(height2)
+//     },2000);
+ })
+function ticker(selector, playTime){
+    var height =   $(selector+' li').css('height');
+    height = '-'+height;
+    $(selector+' li').eq(0).animate({'margin-top': height}, playTime,
+    //animate 에서 설정한 애니메이션을 다 실행한 후 실행되는 함수
+    function(){
+        // $(붙일곳).append(붙일애)  $(붙일애).appendTo(붙일곳)
+        $(selector+' li').eq(0).detach().appendTo(selector+' ul').removeAttr('style');
+    })
+}
+// function ticker2(height2){
+//     $('.news_cast_1').eq(0).animate({'margin-top': height2}, 1000,
+//     function(){
+//         $('.news_cast_1').eq(0).detach().appendTo('.news_casts').removeAttr('style');
+//     })
+// }
 /*$('.r2_btn').click(function(){
     //다음 페이지로 가야할지 이전페이지로 가야할지를 결정하는 함수
     //1이면 다음페이지, -1이면 이전페이지
